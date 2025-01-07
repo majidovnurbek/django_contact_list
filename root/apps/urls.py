695161a5e6fcab4373b210ca098bee1c872ad77c
Nurbek <152709750+majidovnurbek@users.noreply.github.com>
2025-01-07 11:07:02 +0500

    Commit

--- root/apps/urls.py
+++ root/apps/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from apps.views import index
+from apps.views import index,HomeDeleteView
 from django.urls import path
-
 
 urlpatterns = [
     path('', index, name='contact_list'),
-]
+    path('delete/<int:pk>', HomeDeleteView.as_view(), name='delete'),]
 
 
--- root/apps/views.py
+++ root/apps/views.py
@@ -1,6 +1,23 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, UpdateView, ListView
 from django.shortcuts import render
-from apps.models import Person,Job
+
+
+from .models import Person, Job
+
 
 def index(request):
     posts = Person.objects.all()
     return render(request,'contact.html', {'posts': posts})
+
+
+class HomeDeleteView(DeleteView):
+    model = Person
+    pk_url_kwarg = 'pk'
+    success_url = reverse_lazy('contact_list')
+
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
